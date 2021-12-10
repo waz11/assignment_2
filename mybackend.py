8c5ee33682439782855f2924262feeae672993bc
waz11 <ronisr@post.bgu.ac.il>
2021-12-10 17:28:37 +0200

    recommendation function

--- mybackend.py
+++ mybackend.py
@@ -3,6 +3,23 @@ import sqlite3
 import queries as query
 
 
+def get_recommendations(location='', time=0, amount=5):
+    connection = sqlite3.connect('database.db')
+    cursor = connection.cursor()
+
+    query = "SELECT StartStationName FROM BikeShare LIMIT 5;"
+    res = cursor.execute(query).fetchall()
+
+    recommendations = []
+    for r in res:
+        recommendations.append(r)
+    print(recommendations)
+
+    connection.commit()
+    connection.close()
+    return recommendations
+
+
 def db_build():
     # https://www.geeksforgeeks.org/how-to-import-a-csv-file-into-a-sqlite-database-table-using-python/
     connection = sqlite3.connect('database.db')
@@ -25,3 +42,4 @@ def db_build():
 
 if __name__ == '__main__':
     db_build()
+    get_recommendations()
