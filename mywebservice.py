1d2235c9f872e421dd555dd390750310d6ad9372
waz11 <ronisr@post.bgu.ac.il>
2021-12-10 17:03:55 +0200

    webservice

---
+++ mywebservice.py
@@ -0,0 +1,16 @@
+from flask import Flask, request as req, jsonify, send_file
+import mybackend as backend
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def webservice():
+    startlocation = req.args.get('startlocation')
+    timeduration = req.args.get('timeduration')
+    k = req.args.get('k')
+    print(startlocation," ", timeduration)
+    res = ['ron','yuval']
+    return jsonify(res)
+
+if __name__ == '__main__':
+    app.run(debug=True)
