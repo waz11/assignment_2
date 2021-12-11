738f426fb994b5479a808fcd1051641b4591a222
waz11 <ronisr@post.bgu.ac.il>
2021-12-11 20:50:59 +0200

    UI

--- mybackend.py
+++ mybackend.py
@@ -6,28 +6,32 @@ import queries
 def clean_rows(list):
     res = []
     for r in list:
-        res.append(r)
+        print(r)
+        res.append(r.translate("(',)"))
     return res
 
 def get_recommendations(location='', time=0, amount=5):
     query = "SELECT StartStationName FROM BikeShare LIMIT 5;"
     res = Database().select_query(query)
     res = list(chain.from_iterable(res))
-    recommendations = []
-    for r in res:
-        recommendations.append(r)
+    recommendations = clean_rows(res)
     print("recommendations: ",recommendations)
     return recommendations
 
 def is_location_exists(location):
-    connection = sqlite3.connect('database.db')
-    cursor = connection.cursor()
     query = "select StartStationName from BikeShare where StartStationName LIKE '%"+location+"%' limit 1;"
-    res = cursor.execute(query).fetchall()
+    res = Database().select_query(query)
     return len(res)>0
+
+def number_of_locations():
+    query = "select COUNT(DISTINCT StartStationName) from BikeShare;"
+    res = Database().select_query(query)
+    return res[0][0]
 
 
 if __name__ == '__main__':
-    db = Database()
-    db.test()
-
+    # db = Database()
+    # db.test()
+    # get_recommendations()
+    # print(is_location_exists('Oakland Ave'))
+    number_of_locations()
